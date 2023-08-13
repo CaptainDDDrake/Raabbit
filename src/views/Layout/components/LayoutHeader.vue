005a520909277e82e7a4bbbe8f16f34900f30d97
CaptainDDDrake <503400835@qq.com>
2023-08-13 20:18:19 +0800

    pinia优化导航数据-d2p17

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,18 +1,7 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/layout'
-import { onMounted } from 'vue'
-import { ref } from 'vue'
-const categoryList = ref([])
-const getCategory = async () => {
-  // 之所以这么封装，是因为想要的在函数处理前或后做点处理
-  const res = await getCategoryAPI()
-  categoryList.value = res.result
-  console.log(categoryList)
-
-}
-onMounted(() => {
-  getCategory()  
-})
+// 使用 Pinia 中的数据
+import { useCategoryStore } from '@/stores/category.js'
+const categoryStore = useCategoryStore()
 </script>
 
 <template>
@@ -22,7 +11,7 @@ onMounted(() => {
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
       </ul>
