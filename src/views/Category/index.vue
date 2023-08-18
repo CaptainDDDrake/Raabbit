83d3ab6d01346e1d3374a4fa1eee5257bc1e437b
CaptainDDDrake <503400835@qq.com>
2023-08-18 23:32:16 +0800

    一级分类-轮播图的实现d3011

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,9 @@
 import { getCategoryAPI } from '@/apis/category'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { getBannerAPI } from '@/apis/home'
+
+// 1-获取导航栏
 const categoryData = ref({}) // 查看接口文档，发现返回是一个对象
 const route = useRoute()  // 通过useRoute获取id
 const getCategory = async () => {
@@ -10,6 +13,18 @@ const getCategory = async () => {
   categoryData.value = res.result
 }
 onMounted(() => getCategory())
+
+// 2-获取banner
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  bannerList.value = res.result
+  // console.log(res.result)
+}
+
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -18,9 +33,17 @@ onMounted(() => getCategory())
       <!-- 面包屑 -->
       <div class="bread-container">
         <el-breadcrumb separator=">">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: ' /' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src=item.imgUrl alt="">
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -104,4 +127,13 @@ onMounted(() => getCategory())
     padding: 25px 0;
   }
 }
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+    img {
+      width: 100%;
+      height: 500px;
+    }
+  }
 </style>
