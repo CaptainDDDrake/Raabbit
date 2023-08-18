bf3ac0d4a704c804f5e22f38be024cd92dcc28c0
CaptainDDDrake <503400835@qq.com>
2023-08-18 16:46:14 +0800

    home-懒加载指令优化d306

--- src/main.js
+++ src/main.js
@@ -2,38 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import { useIntersectionObserver } from '@vueuse/core'
  
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
-// 测试接口函数
-// import { getCategoryAPI } from './apis/testAPI'
-// getCategoryAPI().then(res => {
-//   console.log(res)
-// })
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 app.mount('#app')
 
-// 定义全局指令
-app.directive('img-lazy', {
-  mounted (el, binding) {
-    // el: 指令绑定的那个元素 img
-    // binding: binding.value 指令等于号后面绑定的表达式的值
-    // console.log(el, binding.value)
-    // 懒加载指令逻辑
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        if(isIntersecting) {
-          // 进入视口区域
-          el.src = binding.value
-          // console.log(isIntersecting)
-        }
-      }
-    )
-  }
-})
