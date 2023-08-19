214f42d1cb7fd872a0589b9420b21539c85be6cd
CaptainDDDrake <503400835@qq.com>
2023-08-19 17:14:27 +0800

    二级分类-列表无限加载实现d405

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -29,9 +29,26 @@ onMounted(() => getGoodList())
 
 // tab切换回调
 const tabChange = () => {
-  console.log('tab切换', reqData.value.sortField)
+  // console.log('tab切换', reqData.value.sortField)
   reqData.value.page = 1
   getGoodList()
+}
+
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  // console.log('lets rushhhh')
+  // 获取下一页的数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    // :infinite-scroll-disabled="disabled"
+    disabled.value = true
+    // console.log('底裤了！')
+    
+  }
 }
 
 </script>
@@ -47,13 +64,14 @@ const tabChange = () => {
         <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
+    <!-- 列表切换 -->
     <div class="sub-container">
       <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
          <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"/>
       </div>
