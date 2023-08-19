3acf34da296998767ca1a46939363d5c3f7815b2
CaptainDDDrake <503400835@qq.com>
2023-08-19 15:16:27 +0800

    二级分类-面包屑导航实现d402

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,6 +1,15 @@
 <script setup>
-
-
+import { getCategoryFilterAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+// 获取面包屑导航数据
+const categoryData = ref([])
+const route = useRoute()
+const getCategoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  categoryData.value = res.result
+}
+onMounted(() => getCategoryData())
 </script>
 
 <template>
