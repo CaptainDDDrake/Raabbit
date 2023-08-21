c7773f0ab38b271ff5e3d48aa8fad8af6619a684
CaptainDDDrake <503400835@qq.com>
2023-08-21 17:33:46 +0800

    登陆-退出登陆业务实现d5010

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,17 @@ export const useUserStore = defineStore('user', () => {
     const res = await loginAPI({ account, password })
     userInfo.value = res.result
   }
+
+  // 退出时用户数据清除函数
+  const clearUserInfo = () => {
+    userInfo.value = {}
+  }
+
   // 3. 以对象的格式把 state 和 action return
   return {
     userInfo,
-    getUserInfo
+    getUserInfo,
+    clearUserInfo
   }
 }, {
   persist: true,
