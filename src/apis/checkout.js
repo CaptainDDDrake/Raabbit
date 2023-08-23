afd45b2d932d069426802285b85825f735a41117
CaptainDDDrake <503400835@qq.com>
2023-08-23 17:24:33 +0800

    支付功能实现d6014

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -1,15 +1,15 @@
-import requset from '@/utils/http'
+import request from '@/utils/http'
 
 // 获取详情接口
 export const getCheckInfoAPI = () => {
-  return requset({
+  return request({
     url: '/member/order/pre'
   })
 }
 
 // 创建订单
 export const createOrderAPI = (data) => {
-  return requset({
+  return request({
     url: '/member/order',
     method: 'POST',
     data
